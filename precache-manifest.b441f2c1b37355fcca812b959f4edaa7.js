4b0514698d0bee4693b42982b4ac6ac7cd60d1fb
zhi <1106010597@qq.com>
2021-08-04 21:28:24 +0800

    deploy

---
+++ precache-manifest.b441f2c1b37355fcca812b959f4edaa7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f1bb21fb34f5b07a03",
+    "url": "/morney-website/css/app.b94f7b2e.css"
+  },
+  {
+    "revision": "1c36feb61ba9e53ba8837ed34933eefb",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "20f1bb21fb34f5b07a03",
+    "url": "/morney-website/js/app.8a2b069e.js"
+  },
+  {
+    "revision": "f894856862cb387bb740",
+    "url": "/morney-website/js/chunk-vendors.e563ea37.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "3cb833301674ab269f5e472d4eaaf1db",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
